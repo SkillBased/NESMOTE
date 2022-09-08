d6cef65a03459848a07c91b488626a5486374d49
SkillBased <70569166+SkillBased@users.noreply.github.com>
2022-09-08 23:16:07 +0300

    separate knn and nng
    added RingQuery and RingNNG (when combined they are equivalent to NeighboirhoodGraph)

--- ct_tests.py
+++ ct_tests.py
@@ -1,35 +1,24 @@
 import numpy as np
 
 from time import time
-from NESMOTE.oversampler import FastNESMOTE, NESMOTE
-from sklearn.datasets import make_moons
 
 from imblearn.datasets import fetch_datasets
 
-from NESMOTE.util import std_euclid_distance, std_euclid_wavg
-from imblearn.over_sampling import SMOTE
+from NESMOTE.util import std_euclid_distance
+from NESMOTE.base import RingNNG, NeighborhoodGraph
 
-from tkinter import Tk, Canvas
 
-nsamples = 400
+dataset = fetch_datasets()["yeast_ml8"]
+data, target = dataset.data, dataset.target
+X = data[target == -1]
 
-sets = fetch_datasets()
-dataset = sets["scene"]
-X, y = dataset.data, dataset.target
-print(len(X))
+ng = NeighborhoodGraph(std_euclid_distance)
+rg = RingNNG(std_euclid_distance, n_jobs=-1)
 
-os = FastNESMOTE(std_euclid_distance, std_euclid_wavg)
 timer = time()
-os.fit(X, y)
-print(f"constuction in {round(time() - timer, 2)}")
+ng.wrap_around(X)
+print(f"done in {round(time() - timer, 3)}")
 timer = time()
-nX, ny = os.resample(X, y)  
-print(f"generation in {round(time() - timer, 2)}")
+rg.construct(X)
+print(f"done in {round(time() - timer, 3)}")
 
-os = NESMOTE(std_euclid_distance, std_euclid_wavg)
-timer = time()
-os.fit(X, y)
-print(f"constuction in {round(time() - timer, 2)}")
-timer = time()
-nX, ny = os.resample(X, y)  
-print(f"generation in {round(time() - timer, 2)}")
--- exp2.py
+++ exp2.py
@@ -1,47 +1,59 @@
 import numpy as np
 
 from imblearn.datasets import fetch_datasets
-from imblearn.under_sampling import AllKNN
 
-from NESMOTE.base import NeighborhoodGraph
-from NESMOTE.util import IndexedCoverTree, NaiveNNG, std_euclid_distance
+from NESMOTE.base import RingQuery
+from NESMOTE.util import std_euclid_distance
+
+from sklearn.neighbors import BallTree
 
 from time import time
 
 
-def run_trial(data, target, goal=-1):
+class Score:
+    def __init__(self) -> None:
+        self.n = 0
+        self._sum = 0
+        self._max = 0
+        self._min = 1e9
+    
+    def add(self, val):
+        self.n += 1
+        self._sum += val
+        if val < self._min:
+            self._min = val
+        if val > self._max:
+            self._max = val
+    
+    def get(self):
+        mean = self._sum / self.n
+        mindiff = mean - self._min
+        maxdiff = self._max - mean
+        return "{:8.4f} +- {:6.4f}".format(mean, max(mindiff, maxdiff))
 
-    print("imblearn ann .. ", end="", flush=True)
 
-    timer = time()
-    imb_knn = AllKNN()
-    imb_knn.fit_resample(data, target)
-    imb_time = time() - timer
+def run_trial(X, nruns=16, k=5):
 
-    print("done, ring method .. ", end="", flush=True)
+    scores = [Score(), Score(), Score(), Score()]
 
-    timer = time()
-    ring = NeighborhoodGraph(std_euclid_distance, data[target == goal])
-    ring.neighbor_split()
-    ring_time = time() - timer
-
-    print("done, cover tree .. ", end="", flush=True)
-
-    timer = time()
-    ct = IndexedCoverTree(std_euclid_distance, data[target == goal])
-    ct.execute(data[target == goal])
-    ct_time = time() - timer
-
-    print("done, naive search .. ", end="", flush=True)
-
-    timer = time()
-    naive = NaiveNNG(std_euclid_distance, data[target == goal])
-    naive.execute(data[target == goal])
-    naive_time = time() - timer
-
-    print("done", flush=True)
-
-    return imb_time, ring_time, ct_time, naive_time
+    for _ in range(nruns):
+        
+        timer = time()
+        bt = BallTree(X)
+        scores[0].add(time() - timer)
+        timer = time()
+        bt.query(X[0].reshape(1, -1), k=k)
+        scores[1].add(time() - timer)
+    
+        timer = time()
+        rg = RingQuery(std_euclid_distance)
+        rg.fit(X)
+        scores[2].add(time() - timer)
+        timer = time()
+        rg.query(X[0], k=k)
+        scores[3].add(time() - timer)
+    
+    return scores[0].get(), scores[1].get(), scores[2].get(), scores[3].get()
 
 
 
@@ -51,16 +63,14 @@ ds_names = ["ecoli", "optical_digits", "abalone", "sick_euthyroid", "spectromete
             "us_crime", "yeast_ml8", "car_eval_4", "thyroid_sick", "wine_quality", "solar_flare_m0",
             "yeast_me2", "ozone_level", "abalone_19"]
 
-ds_names = ["ecoli"]
-
 outfile = open("timers.out", "w")
 
-outfile.write("{:15}    {:12}    {:11}      {:10}    {:12}\n".format("dataset", "imblearn ann", "ring method", "cover tree", "naive search"))
+outfile.write("{:15} {:>18} {:>18} {:>18} {:>18}\n".format("dataset", "ball - prep", "ball - query", "ring - prep", "ring - query"))
 for name in ds_names:
     X, y = datasets[name].data, datasets[name].target
     print(f"running trial {name}: ", end="", flush=True)
-    ann, ring, ct, naive = run_trial(X, y)
-    outfile.write("{:15} {:15.4f} {:15.4f} {:15.4f} {:15.4f}\n".format(name, ann, ring, ct, naive))
-    print(f"trial {name} completed in {round(ann + ring + ct + naive, 3)}s", flush=True)
+    bt_construct, bt_query, ring_cnstruct, ring_query = run_trial(X[y==-1])
+    outfile.write("{:15} {} {} {} {}\n".format(name, bt_construct, bt_query, ring_cnstruct, ring_query))
+    print(f"trial {name} completed", flush=True)
 
 outfile.close()
